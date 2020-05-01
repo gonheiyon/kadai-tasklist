d7d325e07ea52c2dfd25f0c6871edceff119ca01
gon <gon@new-one.co.jp>
2020-05-01 00:27:21 +0000

    tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,18 +1,19 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:show, :edit, :update,:destroy]
   
     def index
-          @tasks = Task.all
+          @tasks = current_user.tasks
     end
           
   def show
   end
   
   def new
-      @task = Task.new
+      @task = current_user.tasks.build
   end
   def create
-         @task = Task.new(task_params)
+         @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = 'タスク が正常に追加されました'
       redirect_to @task
@@ -26,6 +27,7 @@ class TasksController < ApplicationController
   end
 
   def update
+    @task = current_user.tasks.build(task_params)
     if @task.update(task_params)
       flash[:success] = 'タスク は正常に更新されました'
       redirect_to @task
@@ -44,12 +46,16 @@ class TasksController < ApplicationController
 # Strong Parameter
 
   private
-  def set_task
-    @task = Task.find(params[:id])
-  end
   
   def task_params
     params.require(:task).permit(:status,:content)
     end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
 
 end
